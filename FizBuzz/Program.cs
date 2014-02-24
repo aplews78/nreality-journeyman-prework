0ee3d0e1043961669a196acfe52ad80f65578df6
aplews78 <aplews@gmail.com>
2014-02-24 19:00:45 +0200

    Update

--- FizBuzz/Program.cs
+++ FizBuzz/Program.cs
@@ -13,21 +13,21 @@ namespace FizBuzz
             Console.WriteLine("--Press any key to start fizz buzz!");
             Console.ReadKey();
 
-            var c = 0;
-            for (c = 1; c <= 100; c += 1)
+            for (var c = 1; c <= 100; c += 1)
             {
                 if (c % 3 == 0 && c % 5 != 0)
                     Console.WriteLine("Fizz");
-                else if (c % 5 == 0 && c % 3 != 0)           
-                    Console.WriteLine("Buzz");                
-                else if (c % 3 == 0 && c % 5 == 0)                
-                    Console.WriteLine("FizzBuzz");                
-                else                
-                    Console.WriteLine(c);                
+                else if (c % 5 == 0 && c % 3 != 0)
+                    Console.WriteLine("Buzz");
+                else if (c % 3 == 0 && c % 5 == 0)
+                    Console.WriteLine("FizzBuzz");
+                else
+                    Console.WriteLine(c);
             }
 
             Console.WriteLine("--Fizz buzz complete!");
             Console.ReadKey();
+
         }
     }
 }
