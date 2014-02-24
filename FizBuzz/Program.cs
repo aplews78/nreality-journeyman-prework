841c6cf8c089082b4ff3dffb838fc08c8852004e
aplews78 <aplews@gmail.com>
2014-02-24 18:52:40 +0200

    Init
    Initial upload

---
+++ FizBuzz/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizBuzz
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("--Press any key to start fizz buzz!");
+            Console.ReadKey();
+
+            var c = 0;
+            for (c = 1; c <= 100; c += 1)
+            {
+                if (c % 3 == 0 && c % 5 != 0)
+                    Console.WriteLine("Fizz");
+                else if (c % 5 == 0 && c % 3 != 0)           
+                    Console.WriteLine("Buzz");                
+                else if (c % 3 == 0 && c % 5 == 0)                
+                    Console.WriteLine("FizzBuzz");                
+                else                
+                    Console.WriteLine(c);                
+            }
+
+            Console.WriteLine("--Fizz buzz complete!");
+            Console.ReadKey();
+        }
+    }
+}
